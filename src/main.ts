1c417c2bb6ec6dc61dbff70858da89869b169f22
Hossein <77438070+Hossein-Haghshenas@users.noreply.github.com>
2023-09-28 11:23:11 +0330

    Project angular material changes

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,8 @@ import {
   provideProtractorTestingSupport,
 } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 bootstrapApplication(AppComponent, {
-  providers: [provideProtractorTestingSupport()],
+  providers: [provideProtractorTestingSupport(), provideAnimations()],
 }).catch((err) => console.error(err));
