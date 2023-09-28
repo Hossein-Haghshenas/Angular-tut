489cff0548f4dd0660c802c00385f742c2216c39
Hossein <77438070+Hossein-Haghshenas@users.noreply.github.com>
2023-09-28 11:23:58 +0330

    Create search bar component

---
+++ src/app/components/searchbar/searchbar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-searchbar',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './searchbar.component.html',
+  styleUrls: ['./searchbar.component.css'],
+})
+export class SearchbarComponent {}
