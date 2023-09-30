ad4e5a30774187e0caee91e3693bd18e23727380
Hossein <77438070+Hossein-Haghshenas@users.noreply.github.com>
2023-09-30 09:22:53 +0330

    Create housing location component to show houses

--- src/app/components/housing-location/housing-location.component.ts
+++ src/app/components/housing-location/housing-location.component.ts
@@ -1,11 +1,13 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+// Types
 import { HousingLocation } from './housinglocation';
 
 @Component({
   selector: 'app-housing-location',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './housing-location.component.html',
   styleUrls: ['./housing-location.component.css'],
 })
