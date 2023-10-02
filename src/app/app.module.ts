1400e17e51f4a2881b3e981c6545a387c80efcdc
Hossein <77438070+Hossein-Haghshenas@users.noreply.github.com>
2023-10-02 15:31:41 +0330

    Implement in-memory-data service & http client

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http'; // <-- Its for communicating with a remote server over HTTP.
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
-
+// App routing modules
+import { AppRoutingModule } from './app-routing.module'; // <-- Routes lives here
+// Components
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
 import { HeroDetailComponent } from './components/heroDetail/hero-detail-component';
 import { MessagesComponent } from './components/messages/messages.component';
-import { AppRoutingModule } from './app-routing.module';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { PageNotFoundComponent } from './views/page-not-found/page-not-found.component';
+// Data services
+import { InMemoryDataService } from './in-memory-data.service';
+import { HeroSearchComponent } from './components/hero-search/hero-search.component';
 
 @NgModule({
   declarations: [
@@ -18,8 +24,20 @@ import { PageNotFoundComponent } from './views/page-not-found/page-not-found.com
     MessagesComponent,
     DashboardComponent,
     PageNotFoundComponent,
+    HeroSearchComponent,
   ],
-  imports: [BrowserModule, FormsModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
+    // and returns simulated server responses.
+    // Remove it when a real server is ready to receive requests.
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, {
+      dataEncapsulation: false,
+    }),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
