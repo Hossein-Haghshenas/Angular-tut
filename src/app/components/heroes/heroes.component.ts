7b91617ba97d04e013e2b2f2e825c531e82a180d
Hossein <77438070+Hossein-Haghshenas@users.noreply.github.com>
2023-10-02 09:33:06 +0330

    Update heroes component

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 // Fake data
 import { HEROES } from '../../mockHeroes';
 import { Hero } from 'src/app/interfaces/hero';
+// Services
+import { HeroService } from './hero.service';
+import { MessageService } from '../messages/message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,13 +12,19 @@ import { Hero } from 'src/app/interfaces/hero';
   styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent {
-  /*
-  hero = 'Windstorm';
-  */
-  heroes = HEROES;
+  constructor(
+    private heroService: HeroService,
+    private messageService: MessageService
+  ) {}
+
+  heroes: Hero[] = [];
   selectedHero?: Hero;
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
+  }
+
+  ngOnInit(): void {
+    this.getHeroes();
   }
 }
